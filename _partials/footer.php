5d3bb17bad3b28e3694b579eddfaefd4e427757e
HPIE <sunilcverma@gmail.com>
2019-04-28 19:55:18 +0530

    Initial Design with HomePage

---
+++ _partials/footer.php
@@ -0,0 +1,79 @@
+  <!-- Footer -->
+  <footer class="footer">
+    <div class="container">
+      <div class="row">
+        <div class="span4">
+          <div class="widget">
+            <h4>About us</h4>
+            <address>
+					<strong>HPIE</strong><br>
+					Ground Floor, Ols SBI Building<br>
+					Lakkar Bazaar, Shimla, HP 171001<br>
+					<abbr title="Phone">P:</abbr> +91-177-2650606
+					</address>
+
+            <address>
+					<strong>Contact us</strong><br>
+					<a href="mailto:#">info@hpie.in</a>
+					</address>
+          </div>
+        </div>
+        <div class="span4">
+          <div class="widget">
+            <h4>Browse pages</h4>
+            <ul class="nav nav-list regular">
+              <li class="nav-header">More from us</li>
+              <li><a href="#">Work for us</a></li>
+              <li><a href="#">Creative process</a></li>
+              <li><a href="#">Case study</a></li>
+              <li class="nav-header">Quick links</li>
+              <li><a href="#">HPIE awards</a></li>
+              <li><a href="#">Meet the team</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="span4">
+          <div class="widget">
+            <h4>Get email updates</h4>
+            <form class="form-horizontal" action="#" method="post">
+              <fieldset>
+                <p>
+                  Sign up for email updates and we'll plant a tree for you through Trees for the Future.
+                </p>
+
+                <div class="input-prepend input-append">
+                  <input class="span2" id="appendedPrependedInput" type="text" placeholder="Email">
+                  <button class="btn btn-inverse" type="submit">Subscribe!</button>
+                </div>
+              </fieldset>
+            </form>
+            <ul class="social_small">
+              <li class="facebook first"><a href="#" title="Facebook">Facebook</a></li>
+              <li class="twitt"><a href="#" title="Twitter">Twitter</a></li>
+              <li class="googleplus"><a href="#" title="google plus">Google plus</a></li>
+              <li class="flickr"><a href="#" title="flickr">Flickr</a></li>
+              <li class="dribbble"><a href="#" title="Dribbble">Dribbble</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="span6">
+            <p>&copy; HPIE - All right reserved</p>
+          </div>
+          <div class="span6">
+            <div class="pull-right">
+              <div class="credits">
+                   Designed by <a href="https://softctrl.in/">SoftCtrl</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </footer>
---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Start Import Head -->
+<?php
+include('_partials/header.php'); // Includes Header Script
+?>
+<!-- End Import Head -->
+
+<body>
+
+
+	<!-- Start Import Navbar -->
+<?php
+	include('_partials/navbar.php'); // Includes Navbar Script
+?>
+	<!-- End Import Navbar -->
+
+
+  <section id="intro">
+    <div class="jumbotron masthead">
+
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <div class="camera_wrap camera_black_skin" id="camera_wrap_2">
+              <div data-thumb="assets/img/slides/camera/img1.jpg" data-src="assets/img/slides/camera/img1.jpg">
+                <div class="camera_caption fadeFromBottom">
+                  <h2>H.P.I.E</h2>
+                  <div class="hidden-phone">
+                    <p>
+                      HIM PRODUCTIVE INSTIIUTE OF EDUCATION was established on March 2003. 
+					  The Institute of Education has evolved into a model-training center churning out quality accountants to cater to the trade & commerce sector of our economy. 
+					  At H.P.I.E, we believe in the practical and modern approach to the study of finance and accounts, wherein the application area of the commerce practices is also taken care of. 
+					  This is our distinct advantage amongst the number of training centers that have mushroomed out of every nook & corner of the state. 
+					  We are the first Industrial Training Institute of Tally Solutions Pvt. Ltd in Shimla, Himachal Pradesh and we are also providing our services in Himachal Pradesh as the Franchisee of Tally Solutions Pvt. Ltd.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div data-thumb="assets/img/slides/camera/img2.jpg" data-src="assets/img/slides/camera/img2.jpg">
+                <div class="camera_caption fadeFromBottom">
+                  <h2>H.P.I.E - Tally</h2>
+                  <div class="hidden-phone">
+                    <p>
+                      HPIE has also evolved in software development and customization of tally products. Being a <font color="blue"> Five Star Tally Sales and Implimentation Partner </font>
+					  HPIE has not only mastered training on financial accounting but also has mastered providing services on software and technical fronts.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div data-thumb="assets/img/slides/camera/img3.jpg" data-src="assets/img/slides/camera/img3.jpg">
+                <div class="camera_caption fadeFromBottom">
+                  <h2>H.P.I.E - Manpower Solutions</h2>
+                  <div class="hidden-phone">
+                    <p>
+                      We generate trained work force by covering the gap between industry and the unemployed
+					  and supply highly skilled and experienced personnel, 
+					  well conversant with computers and language backed up by in-house technical support.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div data-thumb="assets/img/slides/camera/img4.jpg" data-src="assets/img/slides/camera/img4.jpg">
+                <div class="camera_caption fadeFromBottom">
+                  <h2>H.P.I.E - Advance Trainings</h2>
+                  <div class="hidden-phone">
+                    <p>
+                      HPIE is an authorized training center for Center of Development of Advanced Computing (C-DAC). 
+					  <font color="blue">C-DAC PACE is a government initiative for Advanced Computer Education</font>, hence we provide certified IT Courses for beginner and advance learners at affordable price.
+                    </p>
+                  </div>
+                </div>
+              </div>
+			  
+            </div>
+            <!-- #camera_wrap_1 -->
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+        <div class="tagline">
+          <div class="row">
+            <div class="span6">
+              <div class="tagline_text">
+                <h2>H.P.I.E will <span class="text-success">HELP</span> fulfill your IT needs</h2>
+                <div class="tag_list">
+                  <ul>
+                    <li><span><i class="icon-fire"></i> Fastest Growing IT Service Company in the Region</span></li>
+                    <li><span><i class="icon-gift"></i> Delivers More than Expected at Competitive Cost</span></li>
+                    <li><span><i class="icon-plane"></i> Help your Business Fly and Stay Ahead the Crowd</span></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="span6">
+              <div class="btn-toolbar cta">
+                <a class="btn btn-large btn-danger" href="#"><i class="m-icon-big-swapright m-icon-white"></i> Our pricing</a>
+                <a class="btn btn-large btn-success" href="#"><i class="m-icon-big-swapdown m-icon-white"></i> Download now</a>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <!-- end tagline -->
+
+  <section id="maincontent">
+    <div class="container">
+
+      <div class="row">
+        <div class="span3">
+          <h3 class="heading-success"><span class="btn btn-large btn-success"><i class="m-icon-big-swapright m-icon-white"></i></span>&nbsp;&nbsp;Main features</h3>
+          <p>We have around 25 locations in Himachal Pradesh where we have established computer training centers.
+			<ul>
+				<li>SHIMLA</li>
+				<li>RAMPUR</li>
+				<li>HAMIRPUR</li>
+				<li>BILASPUR</li>
+				<li>NADAUN</li>
+				<li>KANGRA</li>
+				<li>NURPUR</li>
+				<li>DHARMASHALA</li>
+				<li>DHALIARA</li>
+				<li>PALAMPUR</li>
+				<li>NALAGARH</li>
+				<li>SOLAN</li>
+				<li>UNA</li>
+				<li>AMB</li>
+				<li>MANDI</li>
+				<li>CHAMBA</li>
+				<li>GHUMARWIN</li>
+				<li>KULLU</li>
+				<li>SUNDERNAGAR</li>
+			</ul>
+		   </p>
+        </div>
+        <div class="span3">
+          <div class="well well-primary box">
+            <img src="assets/img/icons/box-1-white.png" alt="" />
+            <h3>Quality Policy</h3>
+            <p>
+              Every team member from top to bottom of H.P.I.E is committed to create value for
+			  customer satisfaction and achieve above expectations by deliverance of quality service.
+            </p>
+            <a href="#">Read more</a>
+          </div>
+        </div>
+        <div class="span3">
+          <div class="well well-success box">
+            <img src="assets/img/icons/box-2-white.png" alt="" />
+            <h3>Vision</h3>
+            <p>
+              To evolve and position H.P.I.E as a world-class progressive, cost effective and student friendly
+			  providing Career Oriented computerized training 
+			  Integrating frontiers of Technology Service society specially unemployed youth excels in corporate values.
+            </p>
+            <a href="#">Read more</a>
+          </div>
+        </div>
+        <div class="span3">
+          <div class="well well-warning box">
+            <img src="assets/img/icons/box-3-white.png" alt="" />
+            <h3>Social Commitment</h3>
+            <p>
+              Our organization is working with department of social justice and empowerment to educate orphans on computer and providing them jobs.
+            </p>
+            <a href="#">Read more</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+
+  <section id="bottom">
+    <div class="container">
+
+      <!-- divider -->
+      <div class="row">
+        <div class="span12">
+          <div class="divider"></div>
+        </div>
+      </div>
+      <!-- end divider -->
+
+
+      <div class="row">
+        <div class="span12">
+          <h3><a class="btn btn-large btn-success" href="#"><i class="m-icon-big-swapdown m-icon-white"></i></a> Recent works</h3>
+
+          <div id="latest-work" class="carousel btleft">
+            <div class="carousel-wrapper">
+
+              <ul class="portfolio-home da-thumbs">
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work1.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work2.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work3.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work4.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work5.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work6.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work7.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+                <li>
+                  <div class="thumbnail">
+                    <div class="image-wrapp">
+                      <img src="assets/img/dummies/work8.jpg" alt="Portfolio name" title="" />
+                      <article class="da-animate da-slideFromRight" style="display: block;">
+                        <a class="link_post" href="portfolio-detail.html"><img src="assets/img/icons/link_post_icon.png" alt="" /></a>
+                        <span><a class="zoom" data-pretty="prettyPhoto" href="assets/img/dummies/big1.jpg"><img src="assets/img/icons/zoom_icon.png" alt="Portfolio name" title="Portfolio name" /></a></span>
+                      </article>
+                    </div>
+                    <div class="caption">
+                      <h5>Portfolio name</h5>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+	<!-- Start Import Footer -->
+<?php
+	include('_partials/footer.php'); // Includes Footer Script
+?>
+	<!-- End Import Footer -->
+
+
+
+<!-- Start Import Scripts -->
+<?php
+	include('_partials/scripts.php'); // Includes Scripts Script
+?>
+<!-- End Import Scripts -->
+
+</body>
+
+</html>
